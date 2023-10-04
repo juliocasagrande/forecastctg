08da25ac251d06a6328b5fe0522d3ef89846da71
Julio Lima Casagrande <JLCASAGRANDE@ctgpar.ctgbr.com.br>
2023-10-04 11:42:30 -0300

    ttt

--- 1_💲_Orçamento.py
+++ 1_💲_Orçamento.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # Carregar os dados e ordenar pelo Forecast 2023
-df_original = pd.read_excel("Forecast_actual.xlsx", sheet_name='Resumo CAPEX ')
+df_original = pd.read_excel(r"Forecast_actual.xlsx", sheet_name='Resumo CAPEX ')
 df_original = df_original.sort_values(by='FORECAST 2023', ascending=False)
 
 # Lista com os nomes das colunas que queremos selecionar
